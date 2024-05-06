30cf9a832d1e8c1d42450bf2efe510c7942e6fe5
Nightmare8 <juan.caniguante@sansano.usm.cl>
2024-05-06 09:52:06 -0400

    added script to firefox issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@sections': '/src/sections',
-      '@components': '/src/components',
-      '@assets': '/src/assets',
-      '@data': '/src/data',
-    }
-  }
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@sections": "/src/sections",
+			"@components": "/src/components",
+			"@assets": "/src/assets",
+			"@data": "/src/data",
+		},
+	},
+});
